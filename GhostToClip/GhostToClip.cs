20d714c36817f30b59ceeca0a7e5615b62a976ff
Petr Pivoňka <petrpiv1@gmail.com>
2023-01-20 22:48:28 +0100

    Add GbxToolAPI

---
+++ GhostToClip/GhostToClip.cs
@@ -0,0 +1,19 @@
+﻿using GBX.NET.Engines.Game;
+using GbxToolAPI;
+
+namespace GhostToClip;
+
+public class GhostToClip : Tool, IHasOutput<CGameCtnMediaClip>
+{
+    private readonly CGameCtnGhost ghost;
+
+    public GhostToClip(CGameCtnGhost ghost)
+    {
+        this.ghost = ghost;
+    }
+
+    public CGameCtnMediaClip Produce()
+    {
+        throw new NotImplementedException();
+    }
+}
