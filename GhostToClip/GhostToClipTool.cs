05e37f7b72187d3366b44210e80d219556e849a0
Petr Pivoňka <petrpiv1@gmail.com>
2023-03-03 01:33:45 +0100

    Enhance GhostToClip with TM2020 features

--- GhostToClip/GhostToClipTool.cs
+++ GhostToClip/GhostToClipTool.cs
@@ -17,20 +17,62 @@ public class GhostToClipTool : ITool, IHasOutput<CGameCtnMediaClip>, IConfigurab
 
     public CGameCtnMediaClip Produce()
     {
-        var ghostMediaBlock = CGameCtnMediaBlockGhost.Create(ghost)
+        var ghostBlock = GetGhostBlock();
+
+        var ghostTrack = CGameCtnMediaTrack.Create()
+            .WithName(ghost.GhostNickname ?? "Ghost")
+            .WithBlocks(ghostBlock)
             .ForTMUF()
-            .EndingAt(ghost.RaceTime.GetValueOrDefault(TimeInt32.FromSeconds(3)) + TimeInt32.FromSeconds(3))
             .Build();
 
-        var track = CGameCtnMediaTrack.Create()
-            .WithName(ghost.GhostNickname ?? "Unnamed")
-            .WithBlocks(ghostMediaBlock)
+        var cameraBlock = CGameCtnMediaBlockCameraGame.Create()
+            .WithTimeRange(TimeSingle.Zero, ghost.RaceTime.GetValueOrDefault(TimeInt32.FromSeconds(3)))
+            .ForTM2020()
+            .WithClipEntId(1)
+            .Build();
+
+        var cameraTrack = CGameCtnMediaTrack.Create()
+            .WithName("Camera")
+            .WithBlocks(cameraBlock)
             .ForTMUF()
             .Build();
 
         return CGameCtnMediaClip.Create()
-            .WithTracks(track)
+            .WithTracks(ghostTrack, cameraTrack)
             .ForTMUF()
+            .Build();
+    }
+
+    private CGameCtnMediaBlock GetGhostBlock() => Config.Game switch
+    {
+        EGame.Trackmania => GetTrackmaniaGhostBlock(),
+        EGame.Trackmania2020 => GetTrackmania2020GhostBlock(),
+        _ => throw new NotSupportedException($"Game {Config.Game} is not supported."),
+    };
+
+    private CGameCtnMediaBlockEntity GetTrackmania2020GhostBlock()
+    {
+        if (ghost.RecordData is null)
+        {
+            throw new InvalidOperationException("Ghost record data is null.");
+        }
+
+        return CGameCtnMediaBlockEntity.Create(ghost.RecordData)
+            .WithNoticeRecords(new[] { 1, 0 })
+            .ForTM2020()
+            .WithKeys(new CGameCtnMediaBlockEntity.Key(),
+            new CGameCtnMediaBlockEntity.Key
+            {
+                Time = ghost.RaceTime.GetValueOrDefault(TimeInt32.FromSeconds(3))
+            })
+            .Build();
+    }
+
+    private CGameCtnMediaBlockGhost GetTrackmaniaGhostBlock()
+    {
+        return CGameCtnMediaBlockGhost.Create(ghost)
+            .ForTMUF()
+            .EndingAt(ghost.RaceTime.GetValueOrDefault(TimeInt32.FromSeconds(3)) + TimeInt32.FromSeconds(3))
             .Build();
     }
 }
